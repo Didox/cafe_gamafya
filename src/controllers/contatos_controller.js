fb7876346fb5054231018e17cb86e9542e671c3a
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-05-28 20:19:26 -0300

    criando rota post

--- src/controllers/contatos_controller.js
+++ src/controllers/contatos_controller.js
@@ -1,8 +1,11 @@
 module.exports = {
   index: function(req, res, next) {
     res.render('contatos/index');
+    // res.status(200).send({mensagem: "fale conosco"});
   },
   cadastrar: function(req, res, next) {
+    const {nome, email, comentario} = req.body
+
     res.render('contatos/index');
   }
 };
