0823ec2b0ea733d118e3377d8cc6302fd72abe3e
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-05-28 21:56:33 -0300

    api com content-type

--- src/controllers/contatos_controller.js
+++ src/controllers/contatos_controller.js
@@ -2,6 +2,11 @@ const Contato = require("../models/contatos")
 
 module.exports = {
   index: function(req, res, next) {
+    
+    if(req.headers['content-type'] === "application/json"){
+      return res.status(200).send({mensagem: "API de Contatos"})
+    }
+
     res.render('contatos/index', {erro: undefined, sucesso: undefined});
     // res.status(200).send({mensagem: "fale conosco"});
   },
@@ -10,19 +15,32 @@ module.exports = {
     let erro = undefined
     let sucesso = undefined
 
-    try{
-      new Contato({
-        nome: nome,
-        email: email,
-        comentario: comentario
-      }).salvar();
-
-      sucesso = "Contato cadastrado com sucesso"
+    if(nome === "" || !nome){
+      erro = "Nome obrigatório"
     }
-    catch(e){
-      erro = JSON.stringify(e);
+    else if(email === "" || !email){
+      erro = "Email obrigatório"
+    }
+    else{
+      try{
+        new Contato({
+          nome: nome,
+          email: email,
+          comentario: comentario
+        }).salvar();
+  
+        sucesso = "Contato cadastrado com sucesso"
+      }
+      catch(e){
+        erro = JSON.stringify(e);
+      }
     }
     
+    if(req.headers['content-type'] === "application/json"){
+      if(erro) return res.status(400).send({mensagem: erro})
+      else return res.status(200).send({mensagem: sucesso})
+    }
+
     res.render('contatos/index', {erro: erro, sucesso: sucesso});
   }
 };
